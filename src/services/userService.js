e2d73207e8ef4aa62b38cfce1e32320cc3b31258
“Alyssa <agoody44@gmail.com>
2020-12-14 16:30:52 -0500

    updates base url

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,7 +1,7 @@
 import { setToken, getUserFromToken, removeToken } from './tokenService';
 
 
-const BASE_URL = 'http://localhost:3001/api/users';
+const BASE_URL = 'https://pokemon-project-3.herokuapp.com/api';
 
 function signup(user) {
     return fetch(BASE_URL + '/signup', {
